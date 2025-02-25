a11bd5f592840bf1dab1d7574276a3f169a6837c
nglicloud <dattatraya.jagtap@gmail.com>
2025-02-25 20:49:12 +0530

    updated code

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, CircleCI with Docker and AWS S3!");
+        System.out.println("Hello, from CircleCI with Docker and AWS S3!");
     }
 
     public static int add(int a, int b) {
